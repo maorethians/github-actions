acd84714b193e1d827e9bb732a4abf909672250d
maorethians <apaladian@gmail.com>
2025-01-27 15:48:03 -0500

    feat: add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
