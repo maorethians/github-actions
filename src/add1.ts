8803da7d3cf74edea247218c27fc1291e42205fd
maorethians <apaladian@gmail.com>
2025-02-10 10:30:38 -0500

    feat: duplicate functionality

--- src/add1.ts
+++ src/add1.ts
@@ -1,9 +1,13 @@
 export const add1 = (...numbers: number[])=> {
     let result = 0;
-
     numbers.forEach(number => {
         result += number;
     })
 
-    return result
+    let result1 = 0
+    numbers.forEach(number => {
+        result1 += number;
+    })
+
+    return result1
 }
